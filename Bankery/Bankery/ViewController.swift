5936a723324a260d4f4c5e1b9bacf79680347143
Marilise Morona <marilise@192.168.1.4>
2022-04-25 13:56:30 -0300

    feat: create project in view code

---
+++ Bankery/Bankery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankery
+//
+//  Created by Marilise Morona on 25/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
